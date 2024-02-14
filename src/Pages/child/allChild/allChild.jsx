5a299e7056b09e352d2e54b9a6a3d4049ed0f433
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2024-02-14 20:55:49 +0200

    Update allChild.jsx

--- src/Pages/child/allChild/allChild.jsx
+++ src/Pages/child/allChild/allChild.jsx
@@ -24,67 +24,7 @@ const AllChild = () => {
     toast.error(error);
   }, [error]);
 
-  // if (!isLoading) {
-  //   return <Loader />;
-  // }
 
-  //   if (data) {
-  //     return (
-  //       <div className="allUser  col-lg-10 col-sm-9 col-12 ms-auto p-4">
-  //         <h4 className="text-center pb-4">All Students</h4>
-  //         <Table striped bordered hover className="text-center align-middle">
-  //           <thead>
-  //             <tr>
-  //               <th>#</th>
-  //               <th>Name</th>
-  //               <th>Email</th>
-  //               <th>Birthday</th>
-  //               <th>Image</th>
-  //               <th>Edite</th>
-  //               <th>Delete</th>
-  //             </tr>
-  //           </thead>
-  //           <tbody>
-  //             {data.map((el, index) => (
-  //               <tr key={el.id}>
-  //                 <td>{index + 1}</td>
-  //                 <td>{el.user.full_name}</td>
-  //                 <td>{el.user.email}</td>
-  //                 <td>{el.user.date_of_birth}</td>
-  //                 <td>
-  //                   <img src={el.user.image_url} alt="img" className=" " />
-  //                 </td>
-  //                 <td>
-  //                   <i className="fa-solid fa-user-pen pointer"></i>
-  //                 </td>
-  //                 <td>
-  //                   <i
-  //                     className="fa-solid fa-trash-can pointer"
-  //                     onClick={() => {
-  //                       dispatch(deleteChild(el.id));
-  //                     }}
-  //                   ></i>
-  //                 </td>
-  //               </tr>
-  //             ))}
-  //             <tr>
-  //               <td>2</td>
-  //               <td>Jacob</td>
-  //               <td>Thornton</td>
-  //               <td>@fat</td>
-  //             </tr>
-  //             <tr>
-  //               <td>3</td>
-  //               <td colSpan={2}>Larry the Bird</td>
-  //               <td>@twitter</td>
-  //             </tr>
-  //           </tbody>
-  //         </Table>
-  //       </div>
-  //     );
-  //   } else {
-  //     return <h5 className="w-100 text-center align-self-center ">No Data</h5>;
-  //   }
 
   return (
     <>
